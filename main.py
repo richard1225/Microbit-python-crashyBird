45180d3f748e9fc187f9071d3d7b395960d6c97d
richard1225 <a770123341@qq.com>
2018-09-27 18:46:56 +0800

    v1.0

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from microbit import *
-
-M_ARRAY = [[ '0' for i in range(5)] for j in range(5)]
+import random
+import time
+OBS_ARRAY = [[ '0' for i in range(5)] for j in range(5)]
 SCORE = 0
 BIRD_POINT = 2 # 小鸟初始化在2点
 GAME_OVER = False
+WILL_GEN_NEW = 1 # 当前轮次是否产生新障碍
+SPEED = 1000 # 障碍移动速度，1秒
 obs_last_count = 0
 bird_last_count = 0
 
@@ -11,7 +14,7 @@ def is_crashed(bird_point, obstate_array):
     """
         判断小鸟是否撞毁，如果撞毁，返回True
     """
-    return True if obstate_array[BIRD_POINT][0] == '0' else False
+    return True if obstate_array[BIRD_POINT][0] != '0' else False
 
 def array_to_image(m_array):
     """
@@ -26,7 +29,36 @@ def array_to_image(m_array):
     return Image(res_str)
 
 def obs_move():
-    pass
+    global WILL_GEN_NEW
+    global OBS_ARRAY
+    global GAME_OVER
+    global SCORE
+    new_obs = 0
+
+    # 障碍总体向左移动
+    for line in OBS_ARRAY:
+        for i in range(0,4): # 从 0 到 4-1
+            line[i] = line[i+1]
+
+    if WILL_GEN_NEW == 1:
+        new_obs = random.randint(0,4) # 从 0 到 4
+        
+        obs_line = [ '9' for i in range(5) ]
+        obs_line[new_obs] = '0'
+    else:
+        obs_line = [ '0' for i in range(5) ]
+
+    for i in range(0,5): # 从 0 到 5-1
+        OBS_ARRAY[i][4] = obs_line[i]
+
+    if is_crashed(BIRD_POINT, OBS_ARRAY):
+        GAME_OVER = True
+    else:
+        SCORE += 1
+    WILL_GEN_NEW ^= 1 # 隔次生成障碍
+
+def deepcopy(array):
+    return [i[:] for i in array]
 
 def bird_move(step):
     """
@@ -35,12 +67,14 @@ def bird_move(step):
     global BIRD_POINT
     global GAME_OVER
     if BIRD_POINT + step >= 0 and BIRD_POINT + step <= 4:
-        if is_crashed(BIRD_POINT + step, M_ARRAY):
+        if is_crashed(BIRD_POINT + step, OBS_ARRAY):
             GAME_OVER = True
+            BIRD_POINT += step
+            pass
         else:
             BIRD_POINT += step
 
-initial_array = M_ARRAY[:]
+initial_array = deepcopy(OBS_ARRAY)
 
 while True:
     if obs_last_count == 0:
@@ -54,15 +88,21 @@ while True:
     # start game
     else:
         if not GAME_OVER:
-            tmp = M_ARRAY[:]
+            if running_time() - obs_last_count > SPEED:
+                obs_move()
+                obs_last_count = running_time()
+
+            tmp = deepcopy(OBS_ARRAY)
             tmp[BIRD_POINT][0] = '9'
-            display.show(array_to_image(M_ARRAY))
+            display.show(array_to_image(tmp))
             if button_a.is_pressed():
                 bird_move(1)
+                time.sleep(0.1) 
             elif button_b.is_pressed():
                 bird_move(-1)
+                time.sleep(0.1)  
         else:
-            display.scroll('Score: '+str(SCORE))
+            display.scroll('Score: '+str(SCORE-5))
             break
 
 display.clear()
